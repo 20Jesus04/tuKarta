30d57842825f3ef915c0b7ffd140ffcd910d069b
20Jesus04 <jesusvallecaballero2005@gmail.com>
2025-04-25 23:20:23 +0200

    Estilo y adaptacion de elementos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import { getCartas } from './services/cartasService';
 import { CartaList } from './components/CartaList';
+import "./App.css";
+import logo from './assets/LOGO_TUKARTAsintitulo.png';
+
 
 export const App = () => {
   const [cartas, setCartas] = useState([]);
@@ -23,6 +26,26 @@ export const App = () => {
   
   return (
     <>
+        <header className="appHeader">
+      <div className="logoSeccion">
+        <img src={logo} alt="Logo" className="logo" />
+        <span className="nombreApp">tuKarta</span>
+      </div>
+
+      <div className="buscadorSeccion">
+        <input
+          type="text"
+          className="barraBusqueda"
+          placeholder="Buscar cartas"
+          disabled
+        />
+      </div>
+
+      <div className="authSeccion">
+        <button className="botonAuth">Login</button>
+        <button className="botonAuth">Sign up</button>
+      </div>
+    </header>
        <div className="App">
       {cargando && <p>Cargando cartas...</p>}
       {error && <p style={{ color: 'red' }}>{error}</p>}
